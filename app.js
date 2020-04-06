817a1cbe3d72660dc705c4d039d9697d72efbd30
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-04-06 20:40:35 +0200

    Affichage du jeu, du deck. Création de la classe Joueur, Domino

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/* MADE BY THOMAS HEBRARD
+    AND    EDOUARD LAJOUANIE */
+
+/***************** Création serveur *********************/
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -12,10 +17,13 @@ serv.listen(8000);
 
 console.log("Le server a démarré")
 
-/* Liste des joueurs version socket et player*/
-var SOCKET_LIST = {};
-var PLAYER_LIST = {};
+/********************** Les classes ******************/
 
+/* Liste des sockets des joueurs */
+var SOCKET_LIST = {};
+
+
+/* Classe qui gère les joueurs */
 var Player = function(id){
     var self = {
         id:id,
@@ -23,10 +31,94 @@ var Player = function(id){
     }
     return self;
 }
+/* Liste qui contiendra les joueurs */
+Player.list = {};
+
+/* Fonction qui est lancée quand un joueur se connecte */
+Player.onConnect = function(socket){
+    var player = Player(socket.id);
+    /* On ajoute un listener pour le clic */
+    socket.on('clic', function(date){
+
+    });
+}
+
+/* Fonction lancée quand un joueur se déconnecte */
+Player.onDisconnect = function(socket){
+    /* On l'enlève de la liste des joueurs */
+    delete Player.list[socket.id];
+}
+
+/* Fonction qui met à jour les paramètres du joueur */
+Player.update = function(){
+    var pack = [];
+    for(var p in Player.list){
+        var player = Player.list[i];
+        player.update();
+        pack.push({
+
+        });
+    }
+    return pack;
+}
+
+var Domino = function(id, number_1, number_2){
+    var self = {
+        id:id,
+        number_1:number_1,
+        number_2:number_2,
+        number_used:None,
+        state:"North"
+    };
+
+    self.flip = function(){
+        /* On initialise ce que l'on va envoyer en data pour le socket vers le client */
+        var pack = [];
+
+        /* On garde une trace de l'orientation du domino */
+        if (self.state == "North"){
+            self.state == "East";
+        } else if (self.state == "East"){
+            self.state = "South";
+        } else if (self.state == "South"){
+            self.state = "West";
+        } else if (self.state == "West"){
+            self.state = "North";
+        }
+
+        pack.push({num1:self.number_1,
+            num2:self.number_2,
+            state:self.state
+        });
+        /* On envoit au client pour changer le canvas */
+        socket.emit('flip', pack);
+    }
+
+    self.correspondance = function(Domino){
+        if (self.nu == nbr2){
+            return(true);
+        } else {
+            return(false);
+        }
+    }
+    
+}
+
+/*************** Les fonctions **************/
+
+function getRandomDominos(players, allDominos){
+
+}
+
+
+
+
+
+/************************ Lancement du serveur ***************************/
+
 /* Dès qu'il y a une connection, la function ci-dessous est appelée */
 var io = require('socket.io')(serv, {});
 io.sockets.on('connection', function(socket){
-    console.log('Un joueur s\'est connecté')
 
     /* On associe un id unique à chaque joueur */
     socket.id = Math.random();
@@ -35,9 +127,7 @@ io.sockets.on('connection', function(socket){
     représente un joueur avec des attributs (ici id) */
     SOCKET_LIST[socket.id] = socket;
 
-    /* Et on créé le joueur pour le dissocier de socket */
-    var player = Player(socket.id);
-    PLAYER_LIST[socket.id] = player;
+    Player.onConnect(socket);
 
     /* On écoute à un émit coté client :*/
     /* Si un joueur se déconnecte, la fonction ci dessous sera appelée */
@@ -45,12 +135,14 @@ io.sockets.on('connection', function(socket){
     /* Inutile de mettre un emit côté client, il est programmé automatiquement */
     socket.on('disconnect', function(){
         delete SOCKET_LIST[socket.id];
-        delete PLAYER_LIST[socket.id];
+        Player.onDisconnect(socket);
     });
+
+    socket.emit('drawDeck');
 });
 
 /* Cette fonction sera appellé toutes les 40ms */
 /* Elle va nous permettre de voir qui a gagné*/
 setInterval(function(){
-
+    
 }, 1000/25);
