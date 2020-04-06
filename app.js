6e195b63fa4d690ad3ac56f9622098420a2abe62
Thomas HEBRARD <thomas.hebrard134@gmail.com>
2020-04-06 14:06:33 +0200

    Notification quand un joueur se connecte

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ app.get('/', function(req, res) {
 app.use('/client', express.static(__dirname + '/client'));
 
 serv.listen(8000);
+console.log("Le server a démarré")
+
+/* Dès qu'il y a une connection, la function ci-dessous est appelée*/
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function(socket){
+    console.log('Un joueur s\'est connecté')
+});
